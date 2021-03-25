7e1ff0a258c858e5e894ffe8868892e20fd08f59
Brent Mealhouse <bmealhouse@gmail.com>
2021-03-25 16:46:13 -0500

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Fetches monospace fonts installed on the system.
+ *
+ * @example
+ * getMonospaceFonts();
+ * @returns Monospace font families available
+ */
+export function getMonospaceFonts(): string[];
