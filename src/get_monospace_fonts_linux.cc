6e64c2990a65c236df228331a935f4393cec375f
Brent Mealhouse <bmealhouse@gmail.com>
2021-03-26 13:56:13 -0500

    Remove logging

--- src/get_monospace_fonts_linux.cc
+++ src/get_monospace_fonts_linux.cc
@@ -28,50 +28,34 @@ std::vector<std::string> *getMonospaceFonts() {
 
 
   for (int i = 0; i < fontSet->nfont; i++) {
-    FcChar8 *file, *psName, *style, *familyVal;
+    FcChar8 *familyVal;
     int spacing = 0;
     FcPattern *font = fontSet->fonts[i];
 
     // Get font family
     FcPatternGetString(font, FC_FAMILY, 0, &familyVal);
     std::string family = std::string((char *) familyVal);
-    std::cout << family << std::endl;
-
-    FcPatternGetString(font, FC_FILE, 0, &file);
-    std::cout << "file: " << (char *) file << std::endl;
-
-    FcPatternGetString(font, FC_POSTSCRIPT_NAME, 0, &psName);
-    std::cout << "postscript name: " << (char *) psName << std::endl;
-
-    FcPatternGetString(font, FC_STYLE, 0, &style);
-    std::cout << "style: " << (char *) style << std::endl;
 
     // Get monospace trait
     FcPatternGetInteger(font, FC_SPACING, 0, &spacing);
     bool hasMonospaceTrait = spacing == FC_MONO;
-    std::cout << "hasMonospaceTrait: " << hasMonospaceTrait << std::endl;
 
     // Skip excluded fonts
     index = std::find(std::begin(EXCLUDED_FONTS), std::end(EXCLUDED_FONTS), family);
     if (index != std::end(EXCLUDED_FONTS)) {
-      std::cout << "-> excluded" << std::endl << std::endl;
       continue;
     }
 
     // Skip fonts without a monospace trait that don't exist in the include fonts list
     index = std::find(std::begin(INCLUDED_FONTS_MISSING_MONOSPACE_TRAIT), std::end(INCLUDED_FONTS_MISSING_MONOSPACE_TRAIT), family);
     if (!hasMonospaceTrait && index == std::end(INCLUDED_FONTS_MISSING_MONOSPACE_TRAIT)) {
-      std::cout << "-> not mono & not included" << std::endl << std::endl;
       continue;
     }
 
     // Add font family to the results list if it doesn't already exist
     iterator = std::find(fonts->begin(), fonts->end(), family);
     if (iterator == fonts->end()) {
-      std::cout << "-> pushing to fonts lists" << std::endl << std::endl;
       fonts->push_back(family);
-    } else {
-      std::cout << "-> already exists in font list" << std::endl << std::endl;
     }
   }
 
