c3145518f87aeb99c7dfcb49c6b7b580d8fa2df4
Brent Mealhouse <bmealhouse@gmail.com>
2021-03-26 23:03:23 -0500

    Cleanup linux code

--- src/get_monospace_fonts_linux.cc
+++ src/get_monospace_fonts_linux.cc
@@ -4,9 +4,23 @@
 #include <vector>
 #include "utils.h"
 
-std::vector<std::string> *getMonospaceFonts() {
+using namespace std;
+
+string getFontFamily(FcPattern *font) {
+  FcChar8 *family;
+  FcPatternGetString(font, FC_FAMILY, 0, &family);
+  return string((char *) family);
+}
+
+int getMonospaceTrait(FcPattern *font) {
+  int spacing = 0;
+  FcPatternGetInteger(font, FC_SPACING, 0, &spacing);
+  return spacing;
+}
+
+vector<string> *getMonospaceFonts() {
   // Cache monospace fonts for fast use in future calls
-  static std::vector<std::string> *fonts = NULL;
+  static vector<string> *fonts = NULL;
   if (fonts != NULL) {
     return fonts;
   }
@@ -15,41 +29,35 @@ std::vector<std::string> *getMonospaceFonts() {
 
   // Get system font set
   FcPattern *pattern = FcPatternCreate();
-  FcObjectSet *objectSet = FcObjectSetBuild(FC_FILE, FC_POSTSCRIPT_NAME, FC_FAMILY, FC_STYLE, FC_SPACING, NULL);
+  FcObjectSet *objectSet = FcObjectSetBuild(FC_FAMILY, FC_SPACING, NULL);
   FcFontSet *fontSet = FcFontList(NULL, pattern, objectSet);
-
   if (!fontSet) {
-    return new std::vector<std::string>();
+    return new vector<string>();
   }
 
-  fonts = new std::vector<std::string>();
-  std::vector<std::string>::iterator iterator;
+  fonts = new vector<string>();
 
   for (int i = 0; i < fontSet->nfont; i++) {
-    FcChar8 *familyVal;
-    int spacing = 0;
     FcPattern *font = fontSet->fonts[i];
 
-    // Get font family
-    FcPatternGetString(font, FC_FAMILY, 0, &familyVal);
-    std::string fontFamily = std::string((char *) familyVal);
-
-    // Get monospace trait
-    FcPatternGetInteger(font, FC_SPACING, 0, &spacing);
-    bool hasMonospaceTrait = spacing == FC_MONO;
-
+    string fontFamily = getFontFamily(font);
+    bool hasMonospaceTrait = getMonospaceTrait(font) == FC_MONO;
     if (isExcludedFontFamily(fontFamily, hasMonospaceTrait)) {
       continue;
     }
 
+    vector<string>::iterator iterator= find(
+      fonts->begin(),
+      fonts->end(),
+      fontFamily
+    );
+
     // Add font family to the results list if it doesn't already exist
-    iterator = std::find(fonts->begin(), fonts->end(), fontFamily);
     if (iterator == fonts->end()) {
       fonts->push_back(fontFamily);
     }
   }
 
-  // Cleanup
   FcPatternDestroy(pattern);
   FcObjectSetDestroy(objectSet);
   FcFontSetDestroy(fontSet);
