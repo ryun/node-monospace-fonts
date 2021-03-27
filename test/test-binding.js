2086e931f1e99af0a3d2e53ba8a205ce061b35c4
Brent Mealhouse <bmealhouse@gmail.com>
2021-03-27 10:30:08 -0500

    Add more specific tests

--- test/test-binding.js
+++ test/test-binding.js
@@ -1,14 +1,48 @@
 const { getMonospaceFonts } = require("../binding");
 const assert = require("assert");
+const os = require("os");
 
 assert(getMonospaceFonts, "The expected function is undefined");
 
 function shouldReturnListOfMonospaceFonts() {
   const fonts = getMonospaceFonts();
+
   console.log();
   console.log(fonts.join("\n"));
   console.log();
   assert.ok(fonts.length > 0, "No monospace fonts found");
+
+  switch (os.platform()) {
+    case "darwin":
+      assert.ok(
+        [
+          "Andale Mono",
+          "Courier",
+          "Courier New",
+          "Menlo",
+          "Monaco",
+          "PT Mono",
+        ].every((font) => fonts.includes(font)),
+        "Does not match the expected font list"
+      );
+      break;
+    case "linux":
+      assert.ok(
+        ["DejaVu Sans Mono", "Liberation Mono"].every((font) =>
+          fonts.includes(font)
+        ),
+        "Does not match the expected font list"
+      );
+      break;
+    case "win32":
+      assert.ok(
+        ["Consolas", "Courier New", "Lucida Console"].every((font) =>
+          fonts.includes(font)
+        ),
+        "Does not match the expected font list"
+      );
+      break;
+  }
 }
 
 assert.doesNotThrow(
